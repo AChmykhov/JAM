7a9c5549deded88d42c95ed022b3790258ac08d0
AChmykhov <alexander.chmykhov@gmail.com>
2020-02-10 10:32:11 +0300

    Add Room database infrastructure (pt 1, not ready)

--- app/src/main/java/com/example/jam/MainActivity.kt
+++ app/src/main/java/com/example/jam/MainActivity.kt
@@ -11,6 +11,7 @@ import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.*
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
@@ -70,11 +71,11 @@ class MainActivity : AppCompatActivity() {
 
 
     fun urlEncode(text: ByteArray): String {
-        return URLEncoder.encode(Base64.encodeToString(text, URL_SAFE),"UTF-8")
+        return URLEncoder.encode(Base64.encodeToString(text, URL_SAFE), "UTF-8")
     }
 
     fun urlDecode(text: String): ByteArray {
-        return Base64.decode(URLDecoder.decode(text,"UTF-8"), URL_SAFE)
+        return Base64.decode(URLDecoder.decode(text, "UTF-8"), URL_SAFE)
     }
 
     inner class ReceiverServer @Throws(IOException::class) constructor() : NanoHTTPD(63342) {
@@ -91,9 +92,10 @@ class MainActivity : AppCompatActivity() {
         override fun serve(session: IHTTPSession): Response {
             val params = session.parameters
             when {
-                params.containsKey("message") -> {  //Tyk
+                params.containsKey("message") -> {
                     try {
-                        val externalMessage = decodeMessageFromParams(params, this@MainActivity.keyPair.private)
+                        val externalMessage =
+                            decodeMessageFromParams(params, this@MainActivity.keyPair.private)
                         showMessage("message is `$externalMessage`")
                     } catch (e: java.lang.Exception) {
                         showErrorMessage(e, "message obtaining failed")
@@ -161,7 +163,10 @@ class MainActivity : AppCompatActivity() {
         return cipher.doFinal(symKey)
     }
 
-    fun encryptMessage(messageText: String, symKey: SecretKey): Triple<ByteArray, ByteArray, ByteArray> {
+    fun encryptMessage(
+        messageText: String,
+        symKey: SecretKey
+    ): Triple<ByteArray, ByteArray, ByteArray> {
         val plaintext: ByteArray = messageText.toByteArray(Charsets.UTF_8)
         val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
         cipher.init(Cipher.ENCRYPT_MODE, symKey)
@@ -169,12 +174,18 @@ class MainActivity : AppCompatActivity() {
         return Triple(ciphertext, cipher.iv, symKey.encoded)
     }
 
-    fun encodeAndSend(encodedMessage: Triple<ByteArray, ByteArray, ByteArray>, pubKey: PublicKey, ip: String) {
+    fun encodeAndSend(
+        encodedMessage: Triple<ByteArray, ByteArray, ByteArray>,
+        pubKey: PublicKey,
+        ip: String
+    ) {
         val (encMessage, iv, encodedSymKey) = encodedMessage
         val encryptedSymKey = encryptKey(encodedSymKey, pubKey)
         val stringRequest = StringRequest(
             Request.Method.POST,
-            "http://$ip:63342/?message=${urlEncode(encMessage)}&key=${urlEncode(encryptedSymKey)}&symIv=${urlEncode(iv)}",
+            "http://$ip:63342/?message=${urlEncode(encMessage)}&key=${urlEncode(encryptedSymKey)}&symIv=${urlEncode(
+                iv
+            )}",
             Response.Listener { response ->
                 showMessage("Message sent with code $response")
             },
@@ -193,7 +204,12 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun decryptMessage(ciphertext: ByteArray, iv: ByteArray, encryptedSymKey: ByteArray, privateKey: PrivateKey): String {
+    fun decryptMessage(
+        ciphertext: ByteArray,
+        iv: ByteArray,
+        encryptedSymKey: ByteArray,
+        privateKey: PrivateKey
+    ): String {
         val symKey = decryptKey(encryptedSymKey, privateKey)
         return decryptMessage(ciphertext, iv, symKey)
     }
@@ -206,7 +222,7 @@ class MainActivity : AppCompatActivity() {
         return plaintext.toString(Charsets.UTF_8)
     }
 
-    fun decodeMessageFromParams(params: Map<String, List<String>>, privateKey: PrivateKey) : String {
+    fun decodeMessageFromParams(params: Map<String, List<String>>, privateKey: PrivateKey): String {
         if (!(params.containsKey("message") && params.containsKey("key") && params.containsKey("symIv"))) {
             throw IllegalArgumentException("Missing parameters for incoming message")
         }
@@ -231,7 +247,7 @@ class MainActivity : AppCompatActivity() {
                     )
                     encodeAndSend(encodedMessage, pubKey, ip)
                 } catch (e: java.lang.Exception) {
-                    showErrorMessage(e,"exception in encrypting data")
+                    showErrorMessage(e, "exception in encrypting data")
                 }
             },
             Response.ErrorListener { error ->
@@ -265,4 +281,92 @@ class MainActivity : AppCompatActivity() {
 //        mediaplayer.stop()
         finish()
     }
+}
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    @ColumnInfo(name = "first_name") var firstName: String,
+    @ColumnInfo(name = "last_name") var lastName: String?,
+    @ColumnInfo(name = "publ_key") var publicKey: PublicKey
+)
+
+@Entity(tableName = "messages")
+data class Message(
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    @ColumnInfo(name = "sender_id") var senderId: Int,
+    @ColumnInfo(name = "chat_id") var chatId: Int,
+    @ColumnInfo(name = "text") var text: String
+)
+
+@Entity(tableName = "chats")
+data class Chat(
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    @ColumnInfo(name = "name") var name: String?
+)
+
+@Entity(tableName = "members")
+data class Member(
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    @ColumnInfo(name = "user_id") var userId:Int,
+    @ColumnInfo(name = "chat_id") var chatId: Int
+)
+
+@Dao
+interface JamDao{
+
+//    Users:
+
+    @Query("SELECT * FROM users WHERE id = (:userId)")
+    fun getUserById(userId: Int): User
+
+    @Insert
+    fun insertAll(vararg users: User)
+
+    @Delete
+    fun delete(users: User)
+
+//    Messages
+
+    @Query("SELECT * FROM messages WHERE id = (:messageId)")
+    fun getMessageById(messageId: Int): Message
+
+    @Insert
+    fun insertAll(vararg messages: Message)
+
+    @Delete
+    fun delete(message: Message)
+
+//    Chats
+
+    @Query("SELECT * FROM chats WHERE id = (:chatId)")
+    fun getChatById(chatId: Int): Chat
+
+    @Insert
+    fun insertAll(vararg chats: Chat)
+
+    @Delete
+    fun delete(chat: Chat)
+
+//    Members
+
+    @Query("SELECT * FROM members WHERE id = (:memberId)")
+    fun getMemberById(memberId: Int): Member
+
+    @Insert
+    fun insertAll(vararg members: Member)
+
+    @Delete
+    fun delete(member: Member)
+}
+
+@Database(entities = arrayOf(User::class, Message::class, Chat::class, Member::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun userDao(): JamDao
+
+    val db = Room.databaseBuilder(
+        context.applicationContext,
+        AppDatabase::class, "jam-database"
+    ).build()
 }
