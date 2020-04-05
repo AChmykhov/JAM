edc86391e0f9a36007f0f834639899d7a6908b76
evgenyyayloyan <zhuksar@yandex.ru>
2020-04-05 20:21:26 +0300

    only one task left, dialog appears

--- app/src/main/java/com/example/jam/MainActivity.kt
+++ app/src/main/java/com/example/jam/MainActivity.kt
@@ -1,26 +1,52 @@
 package com.example.jam
 
 import android.os.Bundle
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val contactlist = arrayOf<String>("A", "AA", "AAAA", "AAAAAAAAAAAAAAAAAAAAA")
-
         val recyclerView : RecyclerView = findViewById(R.id.contactList)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = Adapter(generateFakeValues())
+
+    }
+
+    private fun generateFakeValues(): List<String> {
+        val values = mutableListOf<String>()
+        for(i in 0..100) {
+            values.add("$i element")
+        }
+        return values
+    }
+
+    class Adapter(private val values: List<String>): RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+        override fun getItemCount() = values.size
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.contact, parent, false)
+            return ViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder?.textView?.text = values[position]
+        }
+
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            var textView: TextView? = null
+            init {
+                textView = itemView?.findViewById(R.id.contactName)
+            }
+        }
+
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val retValue = super.onCreateOptionsMenu(menu)
@@ -30,8 +56,6 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val cnd = CreateNewDialogFragment()
         val manager = supportFragmentManager
-        val name = ""
-        val IP = ""
         cnd.show(manager, "myDialog")
         return super.onOptionsItemSelected(item)
     }
